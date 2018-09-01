514f442637993a9548498dadfb37855eb1595d64
Seth Miller <s776810@sbuniv.edu>
2018-09-01 01:35:34 +0000

    This is my starting work on program 1.

--- program1.java
+++ program1.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class program1
 {
   public static void main( String [] args )
   {
+    Scanner scan = new Scanner( System.in );
 
+    CharStack opstack = new CharStack();
   }
 }
