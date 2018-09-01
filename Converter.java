9e284626c9ae358c12fe8342c03bac2a7a4a0013
Seth Miller <s776810@sbuniv.edu>
2018-09-01 03:02:37 +0000

    This is my unfinished program 1.

---
+++ Converter.java
@@ -0,0 +1,102 @@
+import java.util.Stack;
+import java.util.Scanner;
+
+public class Converter
+{
+
+  public evaluator
+  {
+
+
+  }// end of evaluator
+
+  public static boolean isOperand ( char c )
+  {
+    if(
+      ( c >= 'a' && c <= 'z' ) ||
+      ( c >= 'A' && c <= 'Z' ) ||
+      ( c >= '0' && c <= '9' ) )
+      {
+        return true;
+
+      }// end of if
+
+      else
+      {
+        return false;
+
+      }// end of else
+
+  }// end of isOperand
+
+  public static boolean isOperator ( char c )
+  {
+    return ( ( c == '+' ) || ( c == '-' ) || ( c == '*' ) || ( c == '/' ) );
+
+  }// end of is Operator
+
+  pubic static int precedenceOrder( char c )
+  {
+    if ( c == '*' || c == '/' )
+    {
+      return 2;
+
+    }// end of if ( c == '*' || c == '/' )
+
+    else
+    {
+      return 1;
+
+    }// end of else
+
+  }// end of precedenceOrder
+
+  public static void main( String [] args )
+  {
+    Scanner scan = new Scanner( System.in );
+    CharStack opstack = new CharStack();
+
+    System.out.print( "Please input your infix expression: " );
+    String input = scan.nextLine();
+    System.out.println();
+
+    for( int i = 0; i > input.length(); i++ )
+    {
+      if ( isOperand( input.charAt(i) ) )
+      {
+        System.out.print( input.charAt(i) );
+
+      }// end of if ( isOperand( input.charAt(i) ) )
+
+      else if ( isOperator( input.charAt(i) ) )
+      {
+
+        opstack.push(); // last line of else if
+
+      }// end of else if ( isOperator( input.charAt(i) ) )
+
+      else if ( input.charAt(i) == '(' )
+      {
+        opstack.push( input.charAt(i) );
+
+      }// end of else if ( input.charAt(i) == '(' )
+
+      else if ( input.charAt(i) == ')' )
+      {
+        char next = opstack.peek();
+        while ( !(next == '(' ) )
+        {
+          System.out.print( next );
+           opstack.pop();
+           next = opstack
+        }
+        opstack.pop
+
+
+      }// end of else if ( input.charAt(i) == ')' )
+
+    }// end of main
+
+  }// end of main
+
+}// end of Converter
